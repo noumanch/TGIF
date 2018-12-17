3b8300f62638fa16ccf2023f4f259c4d8c03b74f
Nouman Shahid <nouman_shahid_gr8@hotmail.com>
2018-12-17 10:30:29 +0100

    adding filters

--- script/main.js
+++ script/main.js
@@ -1,31 +1,89 @@
-var localData = this.data;
+var statistics = {
+  "Democrats": [],
+  "Republicans": [],
+  "Independents": [],
+  "name": "",
+  "novotemissed": "",
+  "pervotemissed": ""
+}
+
+function callingAllFunction() {
+  createTable();
+  //  membersInEachParty()
+}
+callingAllFunction()
 
 function createTable() {
-    let membersData = this.data.results[0].members;
-            let mainDiv = document.getElementById("senate-data");
-    for (i = 0; i < membersData.length; i++) {
-        let tr = document.createElement('tr');
-        mainDiv.append(tr)
-        let name = document.createElement('td');
-        tr.append(name);
-        name.innerHTML = membersData[i].first_name + " " + membersData[i].last_name;
-        let party = document.createElement('td');
-        tr.append(party);
-        party.innerHTML= membersData[i].party;
-        let state = document.createElement('td');
-        tr.append(state);
-        state.innerHTML= membersData[i].state;
-        let year = document.createElement('td');
-        tr.append(year);
-        year.innerHTML= membersData[i].seniority;
-        let percent= document.createElement('td');
-        tr.append(percent);
-        percent.innerHTML=membersData[i].votes_with_party_pct + "%";
-        
+  let membersData = data.results[0].members;
+  for (i = 0; i < membersData.length; i++) {
+    var name = membersData[i].first_name + " " + membersData[i].last_name;
+    var party = membersData[i].party;
+    var state = membersData[i].state;
+    var percentOfVote = membersData[i].votes_with_party_pct + "%";
+    var year = membersData[i].seniority;
 
+    let mainDiv = document.getElementById("senate-data");
+    let tr = document.createElement('tr');
+    mainDiv.append(tr)
 
+    switch (data.results[0].members[i].party) {
+      case "D":
+        statistics.Democrats.push(data.results[0].members[i])
+        break;
+      case "R":
+        statistics.Republicans.push(data.results[0].members[i])
+        break;
+      case "I":
+        statistics.Independents.push(data.results[0].members[i])
     }
 
+    let nametd = document.createElement('td');
+    nametd.innerHTML = name;
+    tr.append(nametd);
+    let partytd = document.createElement('td');
+    partytd.innerHTML = party;
+    tr.append(partytd);
+    let statetd = document.createElement('td');
+    statetd.innerHTML = state;
+    tr.append(statetd);
+    let yeartd = document.createElement('td');
+    yeartd.innerHTML = year;
+    tr.append(yeartd);
+    console.log(yeartd)
+    let percenttd = document.createElement('td');
+    percenttd.innerHTML = percentOfVote;
+    tr.append(percenttd);
+
+    console.log(statistics)
+
+  }
+}
+
+function createfilter() {
+
+  var allDems = document.getElementById(congress).rows;
+
+  var currentstate = document.getElementById("state-filter").value;
+  var checkedCbx = getCheckboxes();
+  for (var i = 0; i < allDems.length; i++) {
+    console.log(checkedCbx)
+    if ((checkedCbx.includes(allDems[i].classList[0]) || checkedCbx.length == 0) && (currentstate == (allDems[i].classList[2]) || currentstate == "")) {
+      allDems[i].style.display = "table-row";
+    } else {
+      allDems[i].style.display = "none";
+    }
+  }
 
 }
-createTable();
+
+function getCheckboxes() {
+  var checks = [];
+  var checkboxes = document.getElementsByClassName("getinfo");
+  var testarray = Array.prototype.slice.call(checkboxes)
+  for (var i = 0; i < testarray.length; i++) {
+    if (testarray[i].checked) {
+      checks.push(testarray[i].value)
+    }
+  }
+  return checks;
+}
